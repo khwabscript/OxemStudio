bf99b3375a9a982ba1b14cabbe6e1b8f88b25f9e
khwabscript <59730462+khwabscript@users.noreply.github.com>
2020-08-21 23:30:39 +0300

    initial commit

--- resources/views/components/input.blade.php
+++
@@ -1,1 +0,0 @@
-<input {{ $attributes->merge(['type' => 'text']) }} name="{{ $key }}" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white @error($key) border-red-500 @else border-gray-200 @enderror" value="{{ old($key, '') }}">
